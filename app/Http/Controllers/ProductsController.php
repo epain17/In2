497dc45d225874c60961ad6c6668cf6a444db2b0
epain17 <epain17@gmail.com>
2018-05-02 11:51:30 +0200

    klart

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -7,6 +7,7 @@ Use App\Store;
 Use App\Review;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class ProductsController extends Controller
 {
@@ -38,20 +39,29 @@ public function inStore($id)
   $productInStores =Product_Store::all()->where('product_id', $id);
   $productReviews = Review::all()->where('product_id', $id);
 
-  $stores = Store::all();
-  $storesWithProduct = [];
-
   $product["reviews"] = $productReviews;
 
-  foreach ($stores as $s)
+
+  // $stores = Store::all();
+  $storesWithProduct = [];
+
+
+  // foreach ($stores as $s)
+  // {
+  //   foreach ($productInStores as $pS)
+  //   {
+  //     if($s->id == $pS->store_id)
+  //     {
+  //       $storesWithProduct[] = $s;
+  //     }
+  //   }
+  // }
+
+  foreach ($productInStores as $pS)
   {
-    foreach ($productInStores as $pS)
-    {
-      if($s->id == $pS->store_id)
-      {
-        $storesWithProduct[] = $s;
-      }
-    }
+    $store = Store::all()->where('id', $pS->store_id)->first();
+    $storesWithProduct[] = $store;
+
   }
 
   $product["stores"] = $storesWithProduct;
@@ -61,9 +71,32 @@ public function inStore($id)
 
 public function create(Request $request)
 {
+  $product = new Product;
+  $product->title = $request->input("title");
+  $product->brand = $request->input("brand");
+  $product->image = $request->input("image");
+  $product->description = $request->input("description");
+  $product->price = $request->input("price");
+  $createdAt = Carbon::now();
+  $product->created_at = $createdAt;
 
-  $products = Product::create($request->all());
-  return response()->json($products, 201);
+  $product->save();
+
+  foreach ($request->input("stores") as $store)
+  {
+    $productInStock = new Product_Store;
+
+    $productInStock->store_id = $store;
+    $productInStock->product_id = $product->id;
+    $productInStock->save();
+  }
+
+
+
+//  $products = Product::create($request->all());
+  return response()->json([
+    "success" => true
+  ]);
 
 }
 public function delete($id)
